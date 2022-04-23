a23f7a008cbd6eb4bc30c63381a99d31233fad44
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-24 00:11:57 +0300

    bg: corrected bugs

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 import random
 
-from pip import main
 from user import User #importing the user class
 
 def main(): #defining the main function
     while True:
         print("Hello! Welcome to Password Locker.")#welcome message
         print("\n")
-        print("navigation shortcode"Press 'nu' to create a new account:'ex' to exit or 'lg' to login") #navigation shortcode
+        print("navigation shortcode: Press 'nu' to create a new account:'ex' to exit or 'lg' to login") #navigation shortcode
         short_code = input().lower() #short_code input
         print("\n") #printing a new line
        
