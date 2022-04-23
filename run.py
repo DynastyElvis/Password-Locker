7e20f431ce1cf536d78ec0945f8d5c23ff8a0b07
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-24 00:09:17 +0300

    added welcome message and shortcode input

--- run.py
+++ run.py
@@ -5,52 +5,9 @@ from user import User #importing the user class
 
 def main(): #defining the main function
     while True:
+        print("Hello! Welcome to Password Locker.")#welcome message
         print("\n")
-        print("Hello! Welcome to Password Locker.")
-        print("\n")
-        print("-"*10)
-        print("\n")
-        print("Use these short codes : cu - create a new user, lg - login, ex - exit")
-        print("\n")
-        short_code = input().lower()
-        if short_code == 'cu':
-            print("New User")
-            print("-"*10)
-            print("\n")
-            print("Enter your name: ")
-            name = input()
-            print("Enter your password: ")
-            password = input()
-            print("\n")
-            print(f"New User {name} created")
-            print("\n")
-            print("-"*10)
-            print("\n")
-            print("Use these short codes : lg - login, ex - exit")
-            print("\n")
-            short_code = input().lower()
-            if short_code == 'lg':
-                print("Login")
-                print("-"*10)
-                print("\n")
-                print("Enter your name: ")
-                name = input()
-                print("Enter your password: ")
-                password = input()
-                print("\n")
-                print(f"Welcome {name}")
-                print("\n")
-                print("-"*10)
-                print("\n")
-                print("Use these short codes : ex - exit")
-                print("\n")
-                short_code = input().lower()
-                if short_code == 'ex':
-                    print("Exiting...")
-                    break
-                else:
-                    print("I really didn't get that. Please use the short codes")
-            else:
-                print("I really didn't get that. Please use the short codes")
-        else:
-            print("I really didn't get that. Please use the short codes")
+        print("navigation shortcode"Press 'nu' to create a new account:'ex' to exit or 'lg' to login") #navigation shortcode
+        short_code = input().lower() #short_code input
+        print("\n") #printing a new line
+       
